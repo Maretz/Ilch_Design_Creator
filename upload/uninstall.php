3edb2bc8935aa97d59b5862e6d436128a65b9846
Marcel Moritz <info@maretz.eu>
2014-12-05 21:20:33 +0100

    Update uninstall.php

--- upload/uninstall.php
+++ upload/uninstall.php
@@ -67,7 +67,7 @@ $loesch = db_query($clean);
                         $erfolg .= '<p></p><p>Die Dateien von dem Modul wurden erfolgreich gel&ouml;scht.</p><p>Es muss nur noch der Ordner include/designs/mar_Creator gel&ouml;scht werden.</p>';
                     }
                     else {
-                        $erfolg .= '<p class="text-danger">Die Installationsdateien konnten nicht automatisch gel&ouml;scht werden.Folgende Dateien sollten entfernt werden:</p><p class="text-danger"><i>uninstall.php</i><br><i>include/admin/designcreator.php</i><br><i>include/admin/templates/designcreator.htm</i><br><i>include/boxes/lastnews_creator.php</i><br><i>include/boxes/lastforum_creator.php</i><br><i>include/boxes/login_creator.php</i><br><i>include/boxes/style_creator.php</i><br><i>include/images/icons/admin/dc.png</i><br><i>Ordner include/designs/mar_Creator</i></p>';
+                        $erfolg .= '<p class="text-danger">Die Moduldateien konnten nicht automatisch gel&ouml;scht werden.Folgende Dateien sollten entfernt werden:</p><p class="text-danger"><i>uninstall.php</i><br><i>include/admin/designcreator.php</i><br><i>include/admin/templates/designcreator.htm</i><br><i>include/boxes/lastnews_creator.php</i><br><i>include/boxes/lastforum_creator.php</i><br><i>include/boxes/login_creator.php</i><br><i>include/boxes/style_creator.php</i><br><i>include/images/icons/admin/dc.png</i><br><i>Ordner include/designs/mar_Creator</i></p>';
                     }
 
                     echo $erfolg;
